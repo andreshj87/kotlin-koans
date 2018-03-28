11eaf3bc305ff74fb4653dba601766c71212b544
Andrés Hernández <andreshj87@gmail.com>
2018-03-28 14:03:06 +0200

    Add task 26 solution

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -16,4 +16,8 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate> {
+    override fun contains(d: MyDate): Boolean {
+        return d >= start && d <= endInclusive
+    }
+}
